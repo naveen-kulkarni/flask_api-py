994f8926b2fb9a1119f3afd6d2496b1695f5c5be
naveen-kulkarni <33716521+naveen-kulkarni@users.noreply.github.com>
2022-11-29 00:57:02 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,25 +1,16 @@
 import sqlite3
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-
-def connect_to_db():
-    conn = sqlite3.connect('demography.db')
-    return conn
-
-def create_db_table():
-    try:
-        conn = connect_to_db()
-        conn.execute('''
-            CREATE TABLE city_population (
-                city_id INTEGER PRIMARY KEY NOT NULL,
-                city TEXT NOT NULL,
-                population INTEGER NOT NULL
-            );
-        ''')
-
-        conn.commit()
-        print("City_population table created successfully")
-    except:
-        print("City_population table creation failed - Maybe table")
-    finally:
+try:
+    conn = sqlite3.connect("demography.sqlite")
+    cursor = conn.cursor()
+    sql_query = """ CREATE TABLE population (
+        id integer PRIMARY KEY,
+        city text NOT NULL,
+        population int NOT NULL
+        )"""
+    cursor.execute(sql_query)
+    conn.commit()
+    print("City_population table created successfully")
+except:
+    print("City_population table creation failed - Maybe table")
+finally:
         conn.close()
