ab746f8cc2c50e4e44ae08f20551054f4bafafa1
naveen-kulkarni <33716521+naveen-kulkarni@users.noreply.github.com>
2022-11-29 23:22:44 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ try:
     conn = sqlite3.connect("demography.sqlite")
     cursor = conn.cursor()
     sql_query = """ CREATE TABLE population (
-        id integer PRIMARY KEY,
-        city text NOT NULL,
-        population int NOT NULL
-        )"""
+                id integer PRIMARY KEY,
+                city text NOT NULL,
+                population int NOT NULL
+                )"""
     cursor.execute(sql_query)
     conn.commit()
     print("City_population table created successfully")
